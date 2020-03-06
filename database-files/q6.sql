4ed1e27353e241a7e85427c2a11ba1e6f33fec1a
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-03-06 14:11:54 -0500

    finished q6

--- database-files/q6.sql
+++ database-files/q6.sql
@@ -1,9 +1,6 @@
-SELECT s.sectionId, i.email, a.outcomeId, a.major, SUM(a.weight) AS `weightTotal`
-	FROM Sections s, Instructors i, Assessments a, OutcomeResults o
-	WHERE o.outcomeId = a.outcomeId /*first two statements match the outcomes and majors */
-		AND o.major = a.major
-		AND o.sectionId = a.sectionId /*these two lines key to section id's*/
-		AND s.sectionId = o.sectionId
-		AND s.instructorId = i.instructorId /* key into instructors */
+SELECT a.sectionId, i.email, a.outcomeId, a.major, SUM(a.weight) AS `weightTotal`
+	FROM Sections s, Instructors i, Assessments a
+	WHERE s.sectionId = a.sectionId AND s.instructorId=i.instructorId
+	GROUP BY a.outcomeId, a.sectionId
 	HAVING SUM(a.weight) != 100
 	ORDER BY i.email ASC, a.major ASC, a.outcomeId ASC;
