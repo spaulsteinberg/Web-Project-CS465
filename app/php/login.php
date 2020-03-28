6f93dc516884cfed08576421a9acac0f3fe522fa
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-03-28 11:18:16 -0400

    finished login template

--- app/php/login.php
+++ app/php/login.php
@@ -20,37 +20,57 @@
             }
             return $conn;
         }
+        //function from W3 schools!
+        function sanitize_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
     ?>
     <?php
         error_reporting(E_ALL); //report errors
         ini_set('display_errors', 1); //set display error mode
 
-        /* SANITIZE!!!!! check if email/pass set */
-        $userEmail = $_GET["email"];
-        $userPassword = $_GET["password"];
-        echo "<p> The email is $userEmail and the password is $userPassword </p>";
+        //check if isset (whether exists or not) or after sanitizing the input is still bad...considers
+        //empty string as true, so also make a check for it.
+        if (!isset($_GET["email"]) || $_GET["email"] == ''){
+            die (json_encode(array('Error' => 'Invalid or empty email')));
+        }
+        else {
+            $userEmail = sanitize_input($_GET["email"]);
+            if (!filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
+                die (json_encode(array('Error' => 'Invalid email')));
+              }
+        }
+        if (!isset($_GET["password"]) || sanitize_input($_GET["password"]) == ''){
+            die (json_encode(array('Error' => 'Invalid password input')));
+        }
+        else {
+            $userPassword = sanitize_input($_GET["password"]);
+        }
         $conn = connect(); //set connection
 
         //set the query up, prepare the query to sanitize, bind the params, then execute. bind the results
         //...the accepted is to see if anything came up, if false the email or pass is incorrect
-        //re-work sql statement for actual use
-        $query = "SELECT firstName, lastName FROM users WHERE email=? AND password=?";
+        //re-work sql statement for actual use!
+        $query = "SELECT firstName, lastName FROM users WHERE email=? AND password=PASSWORD(?)";
         $stmt = $conn->prepare($query);
-        $stmt->bind_param("ss", $userEmail, $userPassword);
+        $stmt->bind_param("ss", $userEmail, $userPassword); //ss for types of binded params
         if ($stmt->execute()) {
             $stmt->bind_result($fname, $lname);
             $accepted = false;
             while ($stmt->fetch()) {
                 $accepted = true;
-                printf("%s %s\n", $fname, $lname);
+                echo json_encode(array('firstName' => $fname, 'lastName' => $lname));
             }
             if(!$accepted){
-                echo "No results!";
+                echo json_encode(array('msg' => 'No results'));
             }
             $stmt->close();
         }
         else {
-            die("Query failed.");
+            echo json_encode(array('msg' => 'Query failed.'));
         }
         $conn->close();
     ?>
